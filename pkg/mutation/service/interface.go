5ddcc033f2ede793679806ebb835a1b7097b9b24
Gianluca De Cicco <gnlcdccc@gmail.com>
2024-03-15 19:35:19 +0100

    First pages commit

--- pkg/mutation/service/interface.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import corev1 "k8s.io/api/core/v1"
-
-// serviceMutator is an interface used to group functions mutating services
-type ServiceMutator interface {
-	Mutate(*corev1.Service) (*corev1.Service, error)
-	Name() string
-}
